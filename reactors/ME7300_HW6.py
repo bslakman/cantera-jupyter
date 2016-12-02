e31bf3eb8fad6deeceef3bd3bba19a0686271c3f
Belinda Slakman <slakman.b@husky.neu.edu>
2016-12-02 18:07:21 -0500

    Prepare the homework.

--- reactors/ME7300_HW6.py
+++ reactors/ME7300_HW6.py
@@ -3,15 +3,15 @@
 
 # # Combustion and Air Pollution HW #6
 # 
-# #### Consider combustion of n-heptane ($C_7H_{16}$). Heptane and air are introduced to a constant volume chamber. First let's consider $\phi$ = 1.0 and atmospheric pressure. Upon ignition, combustion of the premixed fuel/air occurs at constant volume and constant internal energy (assuming that the chamber is adiabatic).
+# #### Consider combustion of propane ($C_3H_{8}$). Propane and air are introduced to a constant volume chamber. First let's consider $\phi$ = 1.0 and atmospheric pressure. Upon ignition, combustion of the premixed fuel/air occurs at constant volume and constant internal energy (assuming that the chamber is adiabatic).
 # 
 # #### Calculate:
-# 1. The adiabatic flame temperature if the fuel and air are preheated and introduced into the chamber at 500 K, for $\phi$ = 0.5, 0.8, 1.0, and 1.3. Plot the result ( $T_{flame}$ vs $\phi$ ) with Excel, or use Python if you are comfortable.
+# 1. The adiabatic flame temperature if the fuel and air are preheated and introduced into the chamber at 750 K, for $\phi$ = 0.5, 0.8, 1.0, and 1.3. Plot the result ( $T_{flame}$ vs $\phi$ ) with Excel, or use Python if you are comfortable.
 # 2. The NO mole fraction at equlibrium for $\phi$ = 0.5, 0.8, 1.0, and 1.3. Plot the result ( $x_{NO}$ vs $\phi$ ) with Excel, or use Python if you are comfortable.
-# 3. The time at which the fuel ignites for $\phi$ =  0.8, 1.0, and 1.3
+# 3. The time at which the fuel ignites for $\phi$ =  0.8, 1.0, and 1.3 (don't do this one by hand)
 # 4. The time at which mole fraction of the fuel drops below 100 ppm for each $\phi$
 # 5. At what time should we quench the reaction (by rapid expansion) if we want to keep NO emissions below 1000 ppm, for each $\phi$? Is this before or after the time in part (4)?
-# 6. Also calculate 1-5 for the $\phi$ = 1.0 case by hand / using STANJAN.
+# 6. Also calculate questions 1,2,4,5 for the $\phi$ = 1.0 case by hand / using STANJAN.
 # 
 # In this homework we will use more detailed combustion chemistry then we previously considered in this class, along with the Zeldovich mechanism for thermal fixation of atmospheric N2, to model and predict NOx formation in heptane combustion.
 # We use an open source software tool called [Cantera](http://www.cantera.org/docs/sphinx/html/index.html) which helps us solve thermodynamics and kinetics problems.
@@ -32,7 +32,7 @@ import seaborn
 get_ipython().magic(u'matplotlib inline')
 
 
-# The n-heptane ($C_7H_{16}$) mechanism we will consider is from the Lawrence Livermore National Lab (LLNL), with the 3-reaction Zeldovich mechanism manually added to it. First we create a Solution object in Cantera for n-heptane gas that includes all of the thermodynamic and kinetic parameters needed to describe its combustion. (Don't worry about the warning)
+# We will consider an n-heptane mechanism we from the Lawrence Livermore National Lab (LLNL), with the 3-reaction Zeldovich mechanism manually added to it. This mechanism includes reactions important in the combustion of propane. First we create a Solution object in Cantera for n-heptane gas that includes all of the thermodynamic and kinetic parameters needed to describe its combustion. (Don't worry about the warning)
 
 # In[ ]:
 
@@ -47,8 +47,8 @@ print "Some of the species in the mechanism:"
 print " ".join(gas.species_names[:20])
 print "\n"
 
-print "Model includes reactions that break down the fuel ('nc7h16'):"
-for i in range(1052,1102,5):
+print "Model includes reactions that break down the fuel ('c3h8'):"
+for i in range(320,340):
     print gas.reaction(i)
 print "\n"
 
@@ -81,22 +81,22 @@ equivalence_ratios = np.array([0.5,0.8,1.0,1.3])
 T = np.zeros_like(equivalence_ratios)
 
 # phi=0.5
-gas.TPX = 500, 1e5, 'nc7h16:1.0,o2:xxxx,n2:xxxx'
+gas.TPX = 750, 1e5, 'c3h8:1.0,o2:xxxx,n2:xxxx'
 gas.equilibrate('UV')
 T[0] = gas.T
 
 # phi=0.8
-gas.TPX = 500, 1e5, 'nc7h16:1.0,o2:xxxx,n2:xxxx'
+gas.TPX = 750, 1e5, 'c3h8:1.0,o2:xxxx,n2:xxxx'
 gas.equilibrate('UV')
 T[1] = gas.T
 
 #phi=1.0
-gas.TPX = 500, 1e5, 'nc7h16:1.0,o2:xxxx,n2:xxxx'
+gas.TPX = 750, 1e5, 'c3h8:1.0,o2:xxxx,n2:xxxx'
 gas.equilibrate('UV')
 T[2] = gas.T
 
 #phi=1.3
-gas.TPX = 500, 1e5, 'nc7h16:1.0,o2:xxxx,n2:xxxx'
+gas.TPX = 750, 1e5, 'c3h8:1.0,o2:xxxx,n2:xxxx'
 gas.equilibrate('UV')
 T[3] = gas.T
             
@@ -113,10 +113,10 @@ equivalence_ratios = np.linspace(0.1, 2, 25)
 x_NO = np.zeros_like(equivalence_ratios)
 temperatures = np.zeros_like(equivalence_ratios)
 for i, phi in enumerate(equivalence_ratios):
-    mol_O2 = 11.0 / phi
+    mol_O2 = 5.0 / phi
     mol_N2 = 3.78 * mol_O2
-    X_string = "nc7h16:1.0,o2:" + str(round(mol_O2, 2)) + ",n2:" + str(round(mol_N2, 2))
-    gas.TPX = 500, 1e5, X_string
+    X_string = "c3h8:1.0,o2:" + str(round(mol_O2, 2)) + ",n2:" + str(round(mol_N2, 2))
+    gas.TPX = 750, 1e5, X_string
     gas.equilibrate('UV')
     temperatures[i] = gas.T
     x_NO[i] = gas['no'].X[0]
@@ -134,8 +134,8 @@ for i, phi in enumerate(equivalence_ratios):
 # In[ ]:
 
 equivalence_ratios = np.array([0.8, 1.0, 1.3])
-ignitions = np.array([70.69, 69.45, 68.19])
-i_fuel = gas.species_names.index('nc7h16')
+ignitions = np.array([25.30, 22.12, 18.91])
+i_fuel = gas.species_names.index('c3h8')
 i_no = gas.species_names.index('no')
 i_oh = gas.species_names.index('oh')
 i_o = gas.species_names.index('o')
@@ -149,10 +149,10 @@ plt_radicals = plt.subplot(3,1,3)
 
 for phi, t in zip(equivalence_ratios, ignitions):
     
-    mol_O2 = 11.0 / phi
+    mol_O2 = 5.0 / phi
     mol_N2 = 3.78 * mol_O2
-    X_string = "nc7h16:1.0,o2:" + str(round(mol_O2, 2)) + ",n2:" + str(round(mol_N2, 2))
-    gas.TPX = 500, 1e5, X_string
+    X_string = "c3h8:1.0,o2:" + str(round(mol_O2, 2)) + ",n2:" + str(round(mol_N2, 2))
+    gas.TPX = 750, 1e5, X_string
 
     reactor = ct.IdealGasReactor(gas)
     reactor_network = ct.ReactorNet([reactor])
@@ -192,7 +192,7 @@ for phi, t in zip(equivalence_ratios, ignitions):
     
     # Print other plots just for stoichiometric combustion
     if phi == 1.0:
-        plt_input_spec.plot(times-t, 1E6*mole_frac[:, i_fuel], label = "C7H14, $\phi$ = 1.0")
+        plt_input_spec.plot(times-t, 1E6*mole_frac[:, i_fuel], label = "C3H8, $\phi$ = 1.0")
         plt_radicals.plot(times-t, 1E6*mole_frac[:,i_no], label = "NO")
         plt_radicals.plot(times-t, 1E6*mole_frac[:,i_oh], label = "OH")
         plt_radicals.plot(times-t, 1E6*mole_frac[:,i_o], label = "O")
